5bcae0765bb9c2aabd1a5f8e4508c4d1fe1d9a08
Frode Nordahl <frode.nordahl@canonical.com>
2023-09-13 19:29:38 +0200

    ovn-tester/ovn_tester.py: Apply black formatting.
    Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>

--- ovn-tester/ovn_tester.py
+++ ovn-tester/ovn_tester.py
@@ -19,34 +19,34 @@ from ovs.stream import Stream
 
 
 GlobalCfg = namedtuple(
-    'GlobalCfg', ['log_cmds', 'cleanup', 'run_ipv4', 'run_ipv6']
+    "GlobalCfg", ["log_cmds", "cleanup", "run_ipv4", "run_ipv6"]
 )
 
-ClusterBringupCfg = namedtuple('ClusterBringupCfg', ['n_pods_per_node'])
+ClusterBringupCfg = namedtuple("ClusterBringupCfg", ["n_pods_per_node"])
 
 
 def usage(name):
     print(
-        f'''
+        f"""
 {name} PHYSICAL_DEPLOYMENT TEST_CONF
 where PHYSICAL_DEPLOYMENT is the YAML file defining the deployment.
 where TEST_CONF is the YAML file defining the test parameters.
-''',
+""",
         file=sys.stderr,
     )
 
 
 def read_physical_deployment(deployment, global_cfg):
-    with open(deployment, 'r') as yaml_file:
+    with open(deployment, "r") as yaml_file:
         dep = yaml.safe_load(yaml_file)
 
-        central_dep = dep['central-node']
+        central_dep = dep["central-node"]
         central_node = PhysicalNode(
-            central_dep.get('name', 'localhost'), global_cfg.log_cmds
+            central_dep.get("name", "localhost"), global_cfg.log_cmds
         )
         worker_nodes = [
             PhysicalNode(worker, global_cfg.log_cmds)
-            for worker in dep['worker-nodes']
+            for worker in dep["worker-nodes"]
         ]
         return central_node, worker_nodes
 
@@ -58,82 +58,82 @@ SSL_CACERT_FILE = "/opt/ovn/pki/switchca/cacert.pem"
 
 
 def read_config(config):
-    global_args = config.get('global', dict())
+    global_args = config.get("global", dict())
     global_cfg = GlobalCfg(**global_args)
 
-    cluster_args = config.get('cluster')
+    cluster_args = config.get("cluster")
     cluster_cfg = ClusterConfig(
-        monitor_all=cluster_args['monitor_all'],
-        logical_dp_groups=cluster_args['logical_dp_groups'],
-        clustered_db=cluster_args['clustered_db'],
-        log_txns_db=cluster_args['log_txns_db'],
-        datapath_type=cluster_args['datapath_type'],
-        raft_election_to=cluster_args['raft_election_to'],
-        node_net=netaddr.IPNetwork(cluster_args['node_net']),
-        n_relays=cluster_args['n_relays'],
-        enable_ssl=cluster_args['enable_ssl'],
-        node_remote=cluster_args['node_remote'],
-        northd_probe_interval=cluster_args['northd_probe_interval'],
-        db_inactivity_probe=cluster_args['db_inactivity_probe'],
-        node_timeout_s=cluster_args['node_timeout_s'],
+        monitor_all=cluster_args["monitor_all"],
+        logical_dp_groups=cluster_args["logical_dp_groups"],
+        clustered_db=cluster_args["clustered_db"],
+        log_txns_db=cluster_args["log_txns_db"],
+        datapath_type=cluster_args["datapath_type"],
+        raft_election_to=cluster_args["raft_election_to"],
+        node_net=netaddr.IPNetwork(cluster_args["node_net"]),
+        n_relays=cluster_args["n_relays"],
+        enable_ssl=cluster_args["enable_ssl"],
+        node_remote=cluster_args["node_remote"],
+        northd_probe_interval=cluster_args["northd_probe_interval"],
+        db_inactivity_probe=cluster_args["db_inactivity_probe"],
+        node_timeout_s=cluster_args["node_timeout_s"],
         internal_net=DualStackSubnet(
-            netaddr.IPNetwork(cluster_args['internal_net'])
+            netaddr.IPNetwork(cluster_args["internal_net"])
             if global_cfg.run_ipv4
             else None,
-            netaddr.IPNetwork(cluster_args['internal_net6'])
+            netaddr.IPNetwork(cluster_args["internal_net6"])
             if global_cfg.run_ipv6
             else None,
         ),
         external_net=DualStackSubnet(
-            netaddr.IPNetwork(cluster_args['external_net'])
+            netaddr.IPNetwork(cluster_args["external_net"])
             if global_cfg.run_ipv4
             else None,
-            netaddr.IPNetwork(cluster_args['external_net6'])
+            netaddr.IPNetwork(cluster_args["external_net6"])
             if global_cfg.run_ipv6
             else None,
         ),
         gw_net=DualStackSubnet(
-            netaddr.IPNetwork(cluster_args['gw_net'])
+            netaddr.IPNetwork(cluster_args["gw_net"])
             if global_cfg.run_ipv4
             else None,
-            netaddr.IPNetwork(cluster_args['gw_net6'])
+            netaddr.IPNetwork(cluster_args["gw_net6"])
             if global_cfg.run_ipv6
             else None,
         ),
         cluster_net=DualStackSubnet(
-            netaddr.IPNetwork(cluster_args['cluster_net'])
+            netaddr.IPNetwork(cluster_args["cluster_net"])
             if global_cfg.run_ipv4
             else None,
-            netaddr.IPNetwork(cluster_args['cluster_net6'])
+            netaddr.IPNetwork(cluster_args["cluster_net6"])
             if global_cfg.run_ipv6
             else None,
         ),
-        n_workers=cluster_args['n_workers'],
-        vips=cluster_args['vips'],
-        vips6=cluster_args['vips6'],
-        vip_subnet=cluster_args['vip_subnet'],
-        static_vips=cluster_args['static_vips'],
-        static_vips6=cluster_args['static_vips6'],
-        use_ovsdb_etcd=cluster_args['use_ovsdb_etcd'],
-        northd_threads=cluster_args['northd_threads'],
+        n_workers=cluster_args["n_workers"],
+        vips=cluster_args["vips"],
+        vips6=cluster_args["vips6"],
+        vip_subnet=cluster_args["vip_subnet"],
+        static_vips=cluster_args["static_vips"],
+        static_vips6=cluster_args["static_vips6"],
+        use_ovsdb_etcd=cluster_args["use_ovsdb_etcd"],
+        northd_threads=cluster_args["northd_threads"],
         ssl_private_key=SSL_KEY_FILE,
         ssl_cert=SSL_CERT_FILE,
         ssl_cacert=SSL_CACERT_FILE,
     )
 
     brex_cfg = BrExConfig(
-        physical_net=cluster_args.get('physical_net', 'providernet'),
+        physical_net=cluster_args.get("physical_net", "providernet"),
     )
 
-    bringup_args = config.get('base_cluster_bringup', dict())
+    bringup_args = config.get("base_cluster_bringup", dict())
     bringup_cfg = ClusterBringupCfg(
-        n_pods_per_node=bringup_args.get('n_pods_per_node', 10)
+        n_pods_per_node=bringup_args.get("n_pods_per_node", 10)
     )
     return global_cfg, cluster_cfg, brex_cfg, bringup_cfg
 
 
 def setup_logging(global_cfg):
-    FORMAT = '%(asctime)s | %(name)-12s |%(levelname)s| %(message)s'
+    FORMAT = "%(asctime)s | %(name)-12s |%(levelname)s| %(message)s"
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=FORMAT)
     logging.Formatter.converter = time.gmtime
 
@@ -162,10 +162,10 @@ def setup_logging(global_cfg):
 
 
 RESERVED = [
-    'global',
-    'cluster',
-    'base_cluster_bringup',
-    'ext_cmd',
+    "global",
+    "cluster",
+    "base_cluster_bringup",
+    "ext_cmd",
 ]
 
 
@@ -175,8 +175,8 @@ def configure_tests(yaml, central_node, worker_nodes, global_cfg):
         if section in RESERVED:
             continue
 
-        mod = importlib.import_module(f'tests.{section}')
-        class_name = ''.join(s.title() for s in section.split('_'))
+        mod = importlib.import_module(f"tests.{section}")
+        class_name = "".join(s.title() for s in section.split("_"))
         cls = getattr(mod, class_name)
         tests.append(cls(yaml, central_node, worker_nodes, global_cfg))
     return tests
@@ -189,12 +189,12 @@ def create_nodes(cluster_config, central, workers):
     external_net = cluster_config.external_net
     gw_net = cluster_config.gw_net
     db_containers = (
-        ['ovn-central-1', 'ovn-central-2', 'ovn-central-3']
+        ["ovn-central-1", "ovn-central-2", "ovn-central-3"]
         if cluster_config.clustered_db
-        else ['ovn-central']
+        else ["ovn-central"]
     )
     relay_containers = [
-        f'ovn-relay-{i + 1}' for i in range(cluster_config.n_relays)
+        f"ovn-relay-{i + 1}" for i in range(cluster_config.n_relays)
     ]
     central_node = CentralNode(
         central, db_containers, relay_containers, mgmt_net, mgmt_ip
@@ -202,7 +202,7 @@ def create_nodes(cluster_config, central, workers):
     worker_nodes = [
         WorkerNode(
             workers[i % len(workers)],
-            f'ovn-scale-{i}',
+            f"ovn-scale-{i}",
             mgmt_net,
             mgmt_ip + i,
             DualStackSubnet.next(internal_net, i),
@@ -245,12 +245,12 @@ def run_base_cluster_bringup(ovn, bringup_cfg, global_cfg):
         ovn.provision_lb_group()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         usage(sys.argv[0])
         sys.exit(1)
 
-    with open(sys.argv[2], 'r') as yaml_file:
+    with open(sys.argv[2], "r") as yaml_file:
         config = yaml.safe_load(yaml_file)
 
     global_cfg, cluster_cfg, brex_cfg, bringup_cfg = read_config(config)
